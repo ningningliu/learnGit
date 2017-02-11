3c3542f4aa3b81f46cdbda73dcf07802e5a88d5a
ningningliu <lnn35@hotmail.com>
2017-02-11 16:04:04 +0000

    new function

--- Test.java
+++ Test.java
@@ -1,5 +1,17 @@
 class Test {
 	public static void main(String[] args) {
-		System.out.println("this is version two");	
+		System.out.println("this is version one");	
+		System.out.println("this is version one");	
+		System.out.println("this is version one");	
+		System.out.println("this is version one");	
+		System.out.println("this is version one");	
+		System.out.println("this is version one");	
+		System.out.println("this is version one");	
+		System.out.println("this is version one");	
+		System.out.println("this is version one");	
+		System.out.println("this is version one");	
+	}
+	public void newFunction() {
+		//this is a new function
 	}
 }
